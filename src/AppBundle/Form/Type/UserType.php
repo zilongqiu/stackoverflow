a22e166573c3c965a52f4c3e7091dffe12af1f2f
Zi-long QIU <zilong.qiu@gmail.com>
2015-07-13 23:29:47 +0200

    Add UserType form

---
+++ src/AppBundle/Form/Type/UserType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('username');
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\User'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'user';
+    }
+}
